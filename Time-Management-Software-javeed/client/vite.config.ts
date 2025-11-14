9ea3aa2751d0dd775b75a4f5fabf967cdf3deff5
Javeed <javeedshaik7346@gmail.com>
2025-11-14 17:55:44 +0530

    changed

--- Time-Management-Software-javeed/client/vite.config.ts
+++ Time-Management-Software-javeed/client/vite.config.ts
@@ -1,11 +1,17 @@
-import path from "path"
+import   * as path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      // process JSX inside plain `.js` files too
+      include: ["src/**/*.{js,jsx,ts,tsx}"]
+    }),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
